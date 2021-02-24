45642d98e29ea96b2f097256c3374bba4e7f8140
Faiq <faiq.019@gmail.com>
2021-02-25 02:01:54 +0500

    Components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,14 +1,22 @@
 import React, { Component } from "react";
 class Counter extends Component {
-  // state = {  }
+  state = {
+    count: 0,
+  };
   render() {
     return (
       <React.Fragment>
         <h1>Hello World</h1>
         <br />
+        <span>{10 + 9}</span>
+        <br />
         <button>Click Me</button>
       </React.Fragment>
     );
+  }
+
+  checkCount() {
+    return this.state.count === 0 ? "Zero" : this.state.count;
   }
 }
 
