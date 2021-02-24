7f6ffc20eac8d8fa86cb3be37552d75e62a3806a
Faiq <faiq.019@gmail.com>
2021-02-25 01:57:01 +0500

    Components

---
+++ src/components/counter.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+class Counter extends Component {
+  // state = {  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <br />
+        <button>Click Me</button>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Counter;
