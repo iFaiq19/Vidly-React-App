c8e855ecaf34450a09946d20a6af426ba8591d16
Faiq <faiq.019@gmail.com>
2021-02-25 02:28:44 +0500

    Components

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,27 +2,25 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imgURL: 'https://picsum.photos/250'
+    imgURL: "https://picsum.photos/250",
+  };
+
+  styles = {
+    fontSize: 14,
+    fontWeight: "bold",
   };
   render() {
     return (
       <React.Fragment>
-        <h1>Hello World</h1>
-        <br />
-        <span>{10 + 9}</span>
-        <br />
-        <button>Click Me</button>
-        <br />
-        <span>{this.checkCount()}</span>
-        <br/>
-        <img src={this.state.imgURL} alt=""/>
+        <span style = {this.styles} className="badge badge-primary m-2">{this.checkCount()}</span>
+        <button className="btn btn-secondary btn-sm ">Increment</button>
       </React.Fragment>
     );
   }
 
   checkCount() {
     const { count } = this.state;
-    return count === 0 ? <h2>Zero</h2> : <h2>{count}</h2>;
+    return count === 0 ? "Zero" : count;
   }
 }
 
