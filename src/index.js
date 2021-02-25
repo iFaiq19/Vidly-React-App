c937c73d7e703a50f1215451e5660eb5105e3ebb
Faiq <faiq.019@gmail.com>
2021-02-25 19:26:17 +0500

    working

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import 'font-awesome/css/font-awesome.css';
+import './components/movies';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
