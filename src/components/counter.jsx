dd7700cc2abb715b8820bec3f8e838fc7bc5a4ef
Faiq <faiq.019@gmail.com>
2021-02-25 22:06:32 +0500

    Back to counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -19,16 +19,22 @@ class Counter extends Component {
         </span>
         <span>
           <button
-            className="btn btn-light btn-sm "
+            className="btn btn-light btn-sm m-2"
             onClick={this.incrementCount}
           >
             +
           </button>
           <button
-            className="btn btn-danger btn-sm "
+            className="btn btn-warning btn-sm m-2"
             onClick={this.decrementCount}
           >
             -
+          </button>
+          <button
+            className="btn btn-danger btn-sm m-2"
+            onClick={() => this.props.onDelete(this.props.id)}
+          >
+            Delete
           </button>
         </span>
       </div>
@@ -45,7 +51,7 @@ class Counter extends Component {
 
   getBadge() {
     let classes = "badge m-2 badge-";
-    classes += this.state.value === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "dark" : "primary";
     return classes;
   }
 
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,17 +4,25 @@ import Counter from "./counter";
 class Counters extends Component {
   state = {
     counters: [
-      { id: 1, value: 4 },
+      { id: 1, value: 0 },
       { id: 2, value: 0 },
-      { id: 3, value: 8 },
+      { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
+  };
+
+  deleteHander = (counter) => {
+      console.log(counter.id);
   };
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value}></Counter>
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            onDelete={this.deleteHander(counter)}
+          ></Counter>
         ))}
       </div>
     );
