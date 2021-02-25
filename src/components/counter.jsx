2f98c0aec6c5202da0fa40f5ac2575a7d949d764
Faiq <faiq.019@gmail.com>
2021-02-25 14:34:19 +0500

    working

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 1,
+    count: 0,
+    // tags: ['tag1', 'tag2', 'tag3']
   };
 
   styles = {
     fontSize: 14,
     fontWeight: "bold",
+    width: 70
   };
 
   render() {
@@ -15,19 +17,33 @@ class Counter extends Component {
         <span style={this.styles} className={this.getBadge()}>
           {this.checkCount()}
         </span>
-        <button
-          className="btn btn-secondary btn-sm "
-          onClick={this.incrementCount()}>
-          Increment
-        </button>
+        <span style={{position:"fixed"}}>
+          <button
+            className="btn btn-light btn-sm "
+            
+            onClick={this.incrementCount}
+          >
+            +
+          </button>
+          <button
+            className="btn btn-danger btn-sm "
+            
+            onClick={this.decrementCount}
+          >
+            -
+          </button>
+        </span>
       </React.Fragment>
     );
   }
 
-  incrementCount() {
-    let { count } = this.state;
-    return count += 1;
-  }
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  decrementCount = () => {
+    this.setState({ count: this.state.count - 1 });
+  };
 
   getBadge() {
     let classes = "badge m-2 badge-";
