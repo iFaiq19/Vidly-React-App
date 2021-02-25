305272ef500a878777b28650da8b9adbecd21477
Faiq <faiq.019@gmail.com>
2021-02-26 01:14:20 +0500

    Counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
+import React, { Component } from "react";
+import NavBar from "./components/navbar";
+import Counters from "./components/counters";
 import "./App.css";
-function App() {
-    return(
-      <React.StrictMode>
-        
-      </React.StrictMode>
-    )
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 0 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const newCounters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters: newCounters });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar></NavBar>
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          ></Counters>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -18,20 +18,14 @@ class Counter extends Component {
         </span>
         <span>
           <button
-            className="btn btn-light btn-sm m-2"
+            className="btn btn-secondary btn-sm m-2"
             onClick={()=> this.props.onIncrement(this.props.counter)}
           >
-            +
-          </button>
-          <button
-            className="btn btn-warning btn-sm m-2"
-            onClick={()=> this.props.onDecrement(this.props.counter)}
-          >
-            -
+            Increment
           </button>
           <button
             className="btn btn-danger btn-sm m-2"
-            onClick={() => this.props.onDelete(this.props.counter.id)}
+            onClick={()=> this.props.onDecrement(this.props.counter)}
           >
             Delete
           </button>
@@ -50,7 +44,7 @@ class Counter extends Component {
 
   getBadge() {
     let classes = "badge m-2 badge-";
-    classes += this.props.counter.value === 0 ? "dark" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
