b0988fe1e6ae6467b0954e81007bb3cbad6ee1dc
Faiq <faiq.019@gmail.com>
2021-02-25 13:31:07 +0500

    working

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,21 +1,38 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0,
-    imgURL: "https://picsum.photos/250",
+    count: 1,
   };
 
   styles = {
     fontSize: 14,
     fontWeight: "bold",
   };
+
   render() {
     return (
       <React.Fragment>
-        <span style = {this.styles} className="badge badge-primary m-2">{this.checkCount()}</span>
-        <button className="btn btn-secondary btn-sm ">Increment</button>
+        <span style={this.styles} className={this.getBadge()}>
+          {this.checkCount()}
+        </span>
+        <button
+          className="btn btn-secondary btn-sm "
+          onClick={this.incrementCount()}>
+          Increment
+        </button>
       </React.Fragment>
     );
+  }
+
+  incrementCount() {
+    let { count } = this.state;
+    return count += 1;
+  }
+
+  getBadge() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   checkCount() {
