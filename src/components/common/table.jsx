3167c62bf856fb0d774b27a9f55db7992aa0635e
Faiq <faiq.019@gmail.com>
2021-02-28 14:27:09 +0500

    extracting table

--- src/components/common/table.jsx
+++ src/components/common/table.jsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+import React from "react";
+import TableHeader from "./tableHeader";
+import TableBody from "./tableBody";
+const Table = (props) => {
+  const { columns, sortColumn, onSort, data } = props;
+  return (
+    <table className="table">
+      <TableHeader
+        columns={columns}
+        sortColumn={sortColumn}
+        onSort={onSort}
+      ></TableHeader>
+      <TableBody
+        data={data}
+        columns={columns}
+      ></TableBody>
+    </table>
+  );
+};
 
-class  extends Component {
-    state = {  }
-    render() { 
-        return (  );
-    }
-}
- 
-export default ;
+export default Table;
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Like from "./common/like";
-import TableHeader from "./common/tableHeader";
-import TableBody from "./common/tableBody";
+import Table from "./common/table";
 
 class MoviesTable extends Component {
   columns = [
@@ -33,17 +32,12 @@ class MoviesTable extends Component {
   render() {
     const { movies, sortColumn, onSort } = this.props;
     return (
-      <table className="table">
-        <TableHeader
-          columns={this.columns}
-          sortColumn={sortColumn}
-          onSort={onSort}
-        ></TableHeader>
-        <TableBody
-          data={movies}
-          columns={this.columns}
-        ></TableBody>
-      </table>
+      <Table
+        data={movies}
+        sortColumn={sortColumn}
+        onSort={onSort}
+        columns={this.columns}
+      ></Table>
     );
   }
 }
