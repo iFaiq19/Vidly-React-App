129367967bb8f1e95750e1c5a00285a748970ded
Faiq <faiq.019@gmail.com>
2021-02-28 15:56:03 +0500

    newCommit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React from "react";
 import "./App.css";
 import Movies from "./components/movies";
+import NavBar from "./components/unused/navbar";
 
 function App() {
   return (
     <React.Fragment>
+      <NavBar></NavBar>
       <Movies></Movies>
     </React.Fragment>
   );
