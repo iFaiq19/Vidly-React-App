78712050b2ffd394824d0bb0b4005aef43226d84
Faiq <faiq.019@gmail.com>
2021-04-11 20:41:34 +0500

    Done with routing

--- src/components/customers.jsx
+++ src/components/customers.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Customers = () => {
-  return <h1>Customers</h1>;
+  return (
+    <div>
+      <h1>Customers</h1>
+      <br />
+      <h4>Nothing to show here yet.</h4>
+    </div>
+  );
 };
 
 export default Customers;
--- src/components/rentals.jsx
+++ src/components/rentals.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Rentals = () => {
-    return ( <h1>Rentals</h1> );
-}
- 
+  return (
+    <div>
+      <h1>Rentals</h1>
+      <br />
+      <h4>Nothing to show here yet.</h4>
+    </div>
+  );
+};
+
 export default Rentals;
