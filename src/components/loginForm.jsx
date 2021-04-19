bee1880f8f6d9357bc5b2f8c26c048a955c32ff0
Faiq <faiq.019@gmail.com>
2021-04-19 16:57:25 +0500

    Forms

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -9,14 +9,14 @@ class LoginForm extends Component {
     e.preventDefault();
   };
 
-  handleChange = (e) => {
+  handleChange = ({currentTarget:input}) => {
     const account = { ...this.state.account };
-    account.username = e.currentTarget.value;
-    // account.password = e.currentTarget.value;
+    account[input.name] = input.value;
     this.setState({ account });
   };
 
   render() {
+    const { username, password } = this.state.account;
     return (
       <div>
         <h1>Login</h1>
@@ -24,9 +24,10 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              value={this.state.account.username}
+              value={username}
               onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
@@ -34,9 +35,10 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="password">Password</label>
             <input
-              value={this.state.account.password}
-            //   onChange={this.handleChange}
+              value={password}
+              onChange={this.handleChange}
               id="password"
+              name="password"
               type="text"
               className="form-control"
             />
