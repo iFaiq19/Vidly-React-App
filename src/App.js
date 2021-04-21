43986a0171b9a7eecbca97610ade2d021be3bba7
Faiq <faiq.019@gmail.com>
2021-04-21 17:35:26 +0500

    Register page

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import notFound from "./components/notFound";
 import NavBar from "./components/common/navBar";
-import movieForm from './components/movieForm';
-import LoginForm from './components/loginForm';
+import movieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
 import "./App.css";
 
 function App() {
   return (
     <React.Fragment>
-    <NavBar/>
-    <main className="container">
-      <Switch>
-        <Route path='/login' component={LoginForm}></Route>
-        <Route path='/movies/:id' component={movieForm}></Route>
-        <Route path="/movies" component={Movies}></Route>
-        <Route path="/customers" component={Customers}></Route>
-        <Route path="/rentals" component={Rentals}></Route>
-        <Route path="/not-found" component={notFound}></Route>
-        <Redirect from="/" exact to="/movies"></Redirect>
-        <Redirect to="/not-found"></Redirect>
-      </Switch>
-    </main>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/register" component={RegisterForm}></Route>
+          <Route path="/login" component={LoginForm}></Route>
+          <Route path="/movies/:id" component={movieForm}></Route>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={notFound}></Route>
+          <Redirect from="/" exact to="/movies"></Redirect>
+          <Redirect to="/not-found"></Redirect>
+        </Switch>
+      </main>
     </React.Fragment>
   );
 }
---
+++ src/components/registerForm.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", name: "" },
+    errors: {},
+  };
+
+  schema = {
+    username: Joi.string()
+      .required()
+      .email({ minDomainSegments: 2, tlds: { allow: ["com", "net"] } })
+      .label("Username"),
+    password: Joi.string().required().min(5).label("Password"),
+  };
+
+  doSubmit = () => {
+    //Call Server
+    console.log("Registered");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("name", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
